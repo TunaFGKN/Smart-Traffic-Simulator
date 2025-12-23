ab58f4f63d09ecc76d4617c94f2afe526126ba19
TunaFGKN <tunafigankaplan496@gmail.com>
2025-12-23 19:37:44 +0300

    main function format

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import gui.SmartCityTraffic;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello, world!");
         try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } catch (Exception ignored) {}
         SwingUtilities.invokeLater(() -> new SmartCityTraffic().setVisible(true));
     }
